e3170c1901ed8e9e3a832d42f0c39166eef2e6f2
Jeevan Joseph <jejoseph@egencia.com>
2018-08-10 14:48:16 -0500

    added recordmap

--- src/python/spark_job.py
+++ src/python/spark_job.py
@@ -4,3 +4,12 @@ conf = SparkConf().setAppName("MyFirstETL")
 sc = SparkContext(conf=conf)
 sc.setLogLevel("WARN")
 
+col_headers = 'ID,Case Number,Date,Block,IUCR,Primary Type,Description,Location Description,Arrest,Domestic,Beat,District,Ward,Community Area,FBI Code,Latitude,Longitude'
+def create_recordmap(raw):
+    returnable = {}
+    cols = col_headers.split(",")
+    records = raw.split(",")
+    
+    for index, value in enumerate(cols):
+        returnable[value] = records[index]
+    return returnable
