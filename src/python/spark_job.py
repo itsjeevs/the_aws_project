559a0b50f9ce37e24e9b9cba425ad8650823ead2
Jeevan Joseph <jejoseph@egencia.com>
2018-08-10 15:26:23 -0500

    first wordcount

--- src/python/spark_job.py
+++ src/python/spark_job.py
@@ -4,6 +4,9 @@ conf = SparkConf().setAppName("MyFirstETL")
 sc = SparkContext(conf=conf)
 sc.setLogLevel("WARN")
 
+
+
+
 col_headers = 'ID,Case Number,Date,Block,IUCR,Primary Type,Description,Location Description,Arrest,Domestic,Beat,District,Ward,Community Area,FBI Code,Latitude,Longitude'
 def create_recordmap(raw):
     returnable = {}
@@ -11,5 +14,21 @@ def create_recordmap(raw):
     records = raw.split(",")
     
     for index, value in enumerate(cols):
-        returnable[value] = records[index]
+        returnable[value.lower()] = records[index]
     return returnable
+    
+    
+crimes_raw = ( sc
+    .textFile("s3://jejoseph-codeacademy-2/crimes/*")
+    .filter(lambda x: col_headers not in x)
+    )
+
+
+mapped = crimes_raw.map(create_recordmap)
+# crimes = crimes_raw.map(lambda x: x.split("\t"))
+
+
+mapped.map(lambda x: x['primary type']).distinct().collect()
+
+
+mapped.map(lambda x: (x['primary type'],1)).reduceByKey(lambda x, y: x+y).collect()
